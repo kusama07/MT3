da0e35d1b3169cd57fdad5b5a1eaa26ade411407
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-08-17 05:58:25 +0900

    02_06完了

--- main.cpp
+++ main.cpp
@@ -19,14 +19,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	AABB aabb1
 	{
-		.min{-0.5f, 0.5f, -0.5f},
+		.min{-0.5f, -0.5f, -0.5f},
 		.max{ 0.0f, 0.0f, 0.0f}
 	};
 
-	AABB aabb2
+	Sphere sphere
 	{
-		.min{ 0.2f, 0.2f, 0.2f},
-		.max{ 1.0f, 1.0f, 1.0f}
+		{1.0f, 1.0f, 1.0f},
+		0.1f
 	};
 
 	uint32_t colorS1 = WHITE;
@@ -99,7 +99,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		viewportMatrix = MakeViewPortMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 
 		//当たり判定
-		if (IsCollision(aabb1, aabb2)) {
+		if (IsCollision(aabb1, sphere)) {
 			colorS1 = RED;
 		}
 		else {
@@ -117,7 +117,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
 
 		DrawAABB(aabb1, worldViewProjectionMatrix, viewportMatrix, colorS1);
-		DrawAABB(aabb2, worldViewProjectionMatrix, viewportMatrix, colorS2);
+
+		DrawShere(sphere, worldViewProjectionMatrix, viewportMatrix, colorS2);
 
 		ImGui::Begin("Debug");
 		ImGui::DragFloat3("cameraTRa", &cameraTranslate.x, 0.1f, -50.0f, 50.0f);
@@ -125,8 +126,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		ImGui::DragFloat3("AABB1min", &aabb1.min.x, 0.1f, -1.0f, 5.0f);
 		ImGui::DragFloat3("AABB1max", &aabb1.max.x, 0.1f, -1.0f, 5.0f);
-		ImGui::DragFloat3("AABB2min", &aabb2.min.x, 0.1f, -1.0f, 5.0f);
-		ImGui::DragFloat3("AABB2max", &aabb2.max.x, 0.1f, -1.0f, 5.0f);
+
+		ImGui::DragFloat3("sphereC", &sphere.center.x, 0.1f, -1.0f, 5.0f);
+		ImGui::DragFloat("sphereR", &sphere.radius, 0.1f, -1.0f, 5.0f);
 		ImGui::End();
 
 		///

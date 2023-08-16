5812e3f2a13f9075bdc8fd2488c9ea522b1fb79c
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-08-17 05:59:41 +0900

    01_02完了

--- MyMath.h
+++ MyMath.h
@@ -4,38 +4,38 @@
 #include "Matrix4x4.h"
 #include <cassert>
 
-	Vector3 Add(const Vector3& v1, const Vector3& v2);
+Vector3 Add(const Vector3& v1, const Vector3& v2);
 
-	Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
 
-	float Dot(const Vector3& v1, const Vector3& v2);
+float Dot(const Vector3& v1, const Vector3& v2);
 
-	Vector3 Cross(const Vector3& v1, const Vector3& v2);
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
 
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 
-	Matrix4x4 Inverse(const Matrix4x4& m);
+Matrix4x4 Inverse(const Matrix4x4& m);
 
-	Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
 
-	Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
 
-	Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
 
-	Matrix4x4 MakeRotateXMatrix(float radian);
+Matrix4x4 MakeRotateXMatrix(float radian);
 
-	Matrix4x4 MakeRotateYMatrix(float radian);
+Matrix4x4 MakeRotateYMatrix(float radian);
 
-	Matrix4x4 MakeRotateZMatrix(float radian);
+Matrix4x4 MakeRotateZMatrix(float radian);
 
-	Matrix4x4 MakeRotateXYZMatrix(const Vector3& rotate);
+Matrix4x4 MakeRotateXYZMatrix(const Vector3& rotate);
 
-	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 
-	Matrix4x4 MakeOrthGraphicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+Matrix4x4 MakeOrthGraphicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
 
-	Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
 
-	Matrix4x4 MakeViewPortMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+Matrix4x4 MakeViewPortMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
 
 
